f1540dbfbc406fd0fdbd1a1c8839a281e23584b3
lj <2069057986@qq.com>
2025-12-25 17:13:11 +0800

    init backend structure of the rag project

---
+++ src/main/java/com/lj/rag/RagMain.java
@@ -0,0 +1,14 @@
+package src.main.java.com.lj.rag;
+
+/**
+ * @ClassName src.main.java.com.lj.rag.RagMain
+ * @Description
+ * @Author Dark Chocolate 2069057986@qq.com
+ * @Date 2025/12/25 11:45
+ * @Version JDK 17
+ */
+public class RagMain {
+    public static void main(String[] args) {
+        System.out.println("hello rag!");
+    }
+}
